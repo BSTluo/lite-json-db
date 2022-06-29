8444a15e3bb53e31d2e0a0e64cabc4411b4d8cfb
BST_luo <36332288+BSTluo@users.noreply.github.com>
2022-06-29 13:11:35 +0800

    update

--- server/new.php
+++ server/new.php
@@ -9,6 +9,6 @@ $json_str = file_get_contents('php://input');
 $num = rand(1,99999999);
 $num2 = rand(1,99999999);
 file_put_contents('./list/'.$num.$num2.'.json', $json_str);
-echo '您的词库码为'.$num.$num2;
+echo $num.$num2;
 
 ?>
